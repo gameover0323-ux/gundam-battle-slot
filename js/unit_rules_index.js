242ae10bd0177d77d7a900d1ee393635e254d081
gameover0323-ux <gameover0323@gmail.com>
2026-04-21 07:13:20 +0900

    Add unit rules for Gundam and Wing Zero

---
+++ js/unit_rules_index.js
@@ -0,0 +1,81 @@
+import {
+  getGundamMcDerivedState,
+  executeGundamMcSpecial,
+  onGundamMcTurnEnd,
+  onGundamMcBeforeSlot,
+  onGundamMcEnemyBeforeSlot,
+  onGundamMcAfterSlotResolved,
+  onGundamMcActionResolved,
+  onGundamMcDamaged,
+  modifyGundamMcTakenDamage,
+  onGundamMcResolveChoice
+} from "./js_unit_rules_gundam_mc.js";
+
+import {
+  getZGundamDerivedState,
+  executeZGundamSpecial,
+  onZGundamTurnEnd,
+  onZGundamBeforeSlot,
+  onZGundamEnemyBeforeSlot,
+  onZGundamAfterSlotResolved,
+  onZGundamActionResolved,
+  onZGundamDamaged,
+  modifyZGundamTakenDamage
+} from "./js_unit_rules_z_gundam.js";
+
+import {
+  getWingZeroDerivedState,
+  canUseWingZeroSpecial,
+  executeWingZeroSpecial,
+  onWingZeroTurnEnd,
+  onWingZeroBeforeSlot,
+  onWingZeroEnemyBeforeSlot,
+  onWingZeroDamaged,
+  onWingZeroAfterSlotResolved,
+  onWingZeroActionResolved,
+  modifyWingZeroTakenDamage,
+  modifyWingZeroEvadeAttempt,
+  onWingZeroResolveChoice
+} from "./js_unit_rules_wing_zero.js";
+
+export const unitRulesMap = {
+  gundam_mc: {
+    getDerivedState: getGundamMcDerivedState,
+    executeSpecial: executeGundamMcSpecial,
+    onTurnEnd: onGundamMcTurnEnd,
+    onBeforeSlot: onGundamMcBeforeSlot,
+    onEnemyBeforeSlot: onGundamMcEnemyBeforeSlot,
+    onAfterSlotResolved: onGundamMcAfterSlotResolved,
+    onActionResolved: onGundamMcActionResolved,
+    onDamaged: onGundamMcDamaged,
+    modifyTakenDamage: modifyGundamMcTakenDamage,
+    onResolveChoice: onGundamMcResolveChoice
+  },
+
+  z_gundam: {
+    getDerivedState: getZGundamDerivedState,
+    executeSpecial: executeZGundamSpecial,
+    onTurnEnd: onZGundamTurnEnd,
+    onBeforeSlot: onZGundamBeforeSlot,
+    onEnemyBeforeSlot: onZGundamEnemyBeforeSlot,
+    onAfterSlotResolved: onZGundamAfterSlotResolved,
+    onActionResolved: onZGundamActionResolved,
+    onDamaged: onZGundamDamaged,
+    modifyTakenDamage: modifyZGundamTakenDamage
+  },
+
+  wing_zero: {
+    getDerivedState: getWingZeroDerivedState,
+    canUseSpecial: canUseWingZeroSpecial,
+    executeSpecial: executeWingZeroSpecial,
+    onTurnEnd: onWingZeroTurnEnd,
+    onBeforeSlot: onWingZeroBeforeSlot,
+    onEnemyBeforeSlot: onWingZeroEnemyBeforeSlot,
+    onDamaged: onWingZeroDamaged,
+    onAfterSlotResolved: onWingZeroAfterSlotResolved,
+    onActionResolved: onWingZeroActionResolved,
+    modifyTakenDamage: modifyWingZeroTakenDamage,
+    modifyEvadeAttempt: modifyWingZeroEvadeAttempt,
+    onResolveChoice: onWingZeroResolveChoice
+  }
+};
